5525dfd2aa01e3874c64a3bf7dfbe3276ea29265
Jay ashworth <washwor1@vols.utk.edu>
2021-08-29 23:35:21 -0400

    started output

--- lib_info.cpp
+++ lib_info.cpp
@@ -5,22 +5,32 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <algorithm>
+
 
 using namespace std;
 
+void output(const map<string,Artist> &Artists);
 
 int main(int argc, char *argv[]) {
     map<string,Artist> Artists;
     vector<string> line;
     string temp;
     ofstream musicf;
+
     musicf.open("argv[1]");
     while (!musicf.eof())
     {
         getline(musicf, temp);
     }
-    
+
 
 
     return 0;
+}
+
+void output(map<string,Artist> &Artists) {
+    for(map<string,Artist>::iterator i = Artists.begin(); i!=Artists.end(); ++i) {
+        
+    }
 }
