90ac3f4952d80a055b9e79ecbfd83f3caa9abec8
Jay ashworth <washwor1@vols.utk.edu>
2021-08-29 21:43:41 -0400

    added structs

--- functions.h
+++ functions.h
@@ -1,3 +1,24 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
+
+struct Song { 
+    string title;
+    int time;  // could also be a string
+    int track;
+};
+
+struct Album {
+    map <int, Song > songs;
+    string name;
+    int time;
+    int nsongs;  // optional variable but makes it easier
+};
+
+struct Artist {
+    map <string, Album > albums;
+    string name;
+    int time;
+    int nsongs;
+};
