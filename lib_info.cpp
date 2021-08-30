87ad1d9f69eedfae1f4648671d742c00a0472d89
Andy <azeng2@vols.utk.edu>
2021-08-29 21:59:25 -0400

    e

--- lib_info.cpp
+++ lib_info.cpp
@@ -3,10 +3,22 @@
 #include <structs.h>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    vector<string> line;
+    string temp;
+    ofstream musicf;
+    musicf.open("argv[1]");
+    while (!musicf.eof())
+    {
+        getline(musicf, temp);
+    }
     
+
+
     return 0;
 }
